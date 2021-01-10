280d2f49d32bf020d0dbc5550206389fec651c52
cskrub33 <cskrub33@naver.com>
2021-01-10 16:33:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intentexample/SubActivity.java
@@ -0,0 +1,32 @@
+package com.example.intentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView tv_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        tv_sub.setText(str);
+
+    }
+}
